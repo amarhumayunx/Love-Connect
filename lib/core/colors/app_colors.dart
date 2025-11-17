0f6da1588e56ed6dcf2d82eea8eeb845547540aa
amarhumayunx <amarhumayun@outlook.com>
2025-11-17 17:37:24 +0500

    Add the Splash Screen and Animation in it and fixes the bugs and also enhance the user experience.

---
+++ lib/core/colors/app_colors.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  // Background Colors
+  static const Color backgroundPink = Color(0xFFFFD6D6);
+
+  // Primary Colors
+  static const Color primaryRed = Color(0xFFE5364B);
+
+  // Text Colors
+  static const Color textDarkPink = Color(0xFF96435D);
+  static const Color textLightPink = Color(0xFFD892A1);
+  static const Color textWhite = Colors.white;
+
+  // Private constructor to prevent instantiation
+  AppColors._();
+}
+
